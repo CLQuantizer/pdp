3afb0e01461670b88201b42fec64daa58966e5a7
eziox <ezioxxk@gmail.com>
2025-06-15 14:48:54 +0200

    go

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,8 @@ import { listArguments } from '$lib/server/db/schema';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({locals}) => {
-    console.log("locals:", JSON.stringify(locals, null, 2))
+    //log d1
+    console.log("d1:", JSON.stringify(locals.d1, null, 2))
     const recentArguments = await listArguments(locals.d1, 1, 5);
     return {
         recentArguments,
