998170b321a3b89aeadc681b06794f372dba9223
eziox <ezioxxk@gmail.com>
2025-06-15 15:39:47 +0200

    go

--- src/routes/pdp/+page.server.ts
+++ src/routes/pdp/+page.server.ts
@@ -2,12 +2,12 @@ import { listArguments } from '$lib/server/db/schema';
 import type { PageServerLoad } from './$types';
 import { count } from 'drizzle-orm';
 import { argumentsTable } from '$lib/server/db/schema';
+import { d1 } from '$lib/server/db/db';
 
-export const load: PageServerLoad = async ({url, locals}) => {
-    console.log("locals:", JSON.stringify(locals, null, 2))
+export const load: PageServerLoad = async ({url}) => {
     const page = Number(url.searchParams.get('page') ?? 1);
-    const argumentsList = await listArguments(locals.d1, page);
-    const totalArguments = await locals.d1.select({ value: count() })
+    const argumentsList = await listArguments(d1, page);
+    const totalArguments = await d1.select({ value: count() })
         .from(argumentsTable).then((res:any) => res[0].value);
 
     return {
