79833406ddb00480d886b50d1807e25a46648a6f
eziox <ezioxxk@gmail.com>
2025-07-15 23:46:14 +0200

    go

--- src/lib/integrations/gemini/client.ts
+++ src/lib/integrations/gemini/client.ts
@@ -1,4 +1,4 @@
-import {createGoogleGenerativeAI} from "@ai-sdk/google";
-import {AI} from "$env/static/private";
+// import {createGoogleGenerativeAI} from "@ai-sdk/google";
+// import {AI} from "$env/static/private";
 
-export const GEMINI_POWER = createGoogleGenerativeAI({apiKey:AI})("gemini-2.0-flash-001");
+// export const GEMINI_POWER = createGoogleGenerativeAI({apiKey:AI})("gemini-2.0-flash-001");
