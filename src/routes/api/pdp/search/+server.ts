779d036a4b06912cc0abb0d9c478ce05da118cb0
eziox <ezioxxk@gmail.com>
2025-07-15 23:44:12 +0200

    go

--- src/routes/api/pdp/search/+server.ts
+++ src/routes/api/pdp/search/+server.ts
@@ -4,7 +4,7 @@ import { json } from "@sveltejs/kit";
 export const GET = async ({ url }) => {
     const query = url.searchParams.get("query");
     // @ts-ignore
-    const answer = await env.AI.autorag("pdp-rag").aiSearch({
+    const answer = await env.WORKERS_AI.autorag("pdp-rag").aiSearch({
         query: query,
     });
     return json(answer);
